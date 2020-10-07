bd1fcd1176651f6d803192fa5b28985e9436322a
Gideon Ablorh <Gideon Ablorh>
2020-10-07 21:50:51 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Online from "./OnlineShopping";
+import Payment from "./PaymentSuccessful";
+import AddToCart from "./AddToCart";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* <Online/>
+      <Payment/> */}
+      <AddToCart/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
